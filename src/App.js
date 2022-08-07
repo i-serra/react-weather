e03eb3b719bd3e785ff52a3a6d79dca3ee6e1add
i-serra <104382671+i-serra@users.noreply.github.com>
2022-08-07 12:21:21 +0100

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div>
-
+      <footer>
+        This project was coded by{" "}
+        <a href="https://golden-pasca-71ada2.netlify.app/" target="_blank">
+          Inês Serra
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/i-serra/react-weather" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
