81c8ced9a55395264ebfc20ce24a7a320ba4492d
i-serra <104382671+i-serra@users.noreply.github.com>
2022-08-07 23:51:41 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Weather app</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://golden-pasca-71ada2.netlify.app/" target="_blank" rel="noreferrer">
+        <a href="https://golden-pasca-71ada2.netlify.app/" target="_blank" rel="noopener noreferrer">
           Inês Serra
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/i-serra/react-weather" target="_blank" rel="noreferrer">
+        <a href="https://github.com/i-serra/react-weather" target="_blank" rel="noopener noreferrer">
           open-sourced on GitHub
         </a>
       </footer>
